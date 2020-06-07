c10aa1e97edfd035357b640e5cea450cf03686df
Pratheek_b <pratheek.b@gmail.com>
2020-06-07 01:04:20 -0700

    NavBar changes

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Nav, Navbar, NavDropdown, Form, FormControl } from 'react-bootstrap';
+//import { Button, Nav, Navbar, NavDropdown, Form, FormControl } from 'bootstrap';
 //import './Hello.css';
 // import React, { component } from 'react';
 
@@ -7,27 +7,41 @@ import { Button, Nav, Navbar, NavDropdown, Form, FormControl } from 'react-boots
 const Dropdown = () => {
   return (
   //  <h1> Hello World </h1>
-  <Navbar fixed='top' bg="light" expand="lg">
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-success">Search</Button>
-      </Form>
-    </Navbar.Collapse>
-  </Navbar>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="public/index.html">Navbar</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="public/index.html">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="public/index.html">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="public/index.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="public/index.html">Action</a>
+            <a className="dropdown-item" href="public/index.html">Another action</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="public/index.html">Something else here</a>
+          </div>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href="public/index.html" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="form-inline my-2 my-lg-0">
+        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
 );
 }
 export default Dropdown;
